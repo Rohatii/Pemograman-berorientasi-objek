28a2a995c90488875168dae4011c95a9917b3b23
ROHATI <102013567+Rohatii@users.noreply.github.com>
2022-09-27 15:20:07 +0800

    Add files via upload

---
+++ Biodata.java
@@ -0,0 +1,21 @@
+public class Biodata {
+    public static void main(String[] args) {
+        String namadpn = "Roh";
+        String namablk = "ati";
+        String dsn = "setanggor";
+        String jk = "perempuan";
+        int tgl=25;
+        int bln=07;
+        int thn1=2002;
+        int thn2=2022;
+        int thnow=thn2-thn1;
+
+
+
+        System.out.println("Nama Lengkap : "+ namadpn+namablk);
+        System.out.println("tanggal lahir : "+dsn+" "+tgl+"/"+bln+"/"+thn1);
+        System.out.println("umur : "+thnow+" "+"tahun" );
+        System.out.println("jenis kelamin : "+jk);
+
+    }
+}
